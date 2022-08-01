84ce509215dee2c1e16072f875f50e09e158b364
ayelenleclerc <ayelenleclerc@gmail.com>
2022-08-01 09:54:04 -0300

    clase 03 completada

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ const btn = document.querySelector("[data-form-btn]");
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   const input = document.querySelector("[data-form-input]");
-  console.log(input.value);
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  const content = ` <div>
+                     <i class="far fa-check-square icon"></i>
+                     <span class="task">${value}</span>
+                  </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+  list.appendChild(task);
+  console.log(content);
 });
